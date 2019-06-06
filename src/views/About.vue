779d7233b67610ede21f6a12295b8099e9fb9d9f
LuanComputacao <eng.luanroger@gmail.com>
2019-06-06 00:45:00 -0300

    home and about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>TypeType</h1>
+    <p>It's an app to run against other typists over the world.</p>
+    <h1>Repository</h1>
+    <a href="https://github.com/LuanComputacao/typetype" target="_blank" rel="noopener noreferrer">https://github.com/LuanComputacao/typetype</a>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Are you a good Typist?</p>
+    <p>How many words per minute can you type?</p>
+    <p>Discover it!!!</p>
+    <div>
+      <router-link :to="{name: 'run'}">
+        Run now
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
