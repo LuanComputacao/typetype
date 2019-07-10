ff732a1a66d0224fd49211aaee0ace253d8e2955
LuanComputacao <eng.luanroger@gmail.com>
2019-07-10 02:44:28 -0300

    deploy

---
+++ precache-manifest.b131d89b6bae01cff970df6b2d30b79a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "60dc88a5e88773db4e58",
+    "url": "/typetype/js/about.f09e291d.js"
+  },
+  {
+    "revision": "7cc4430c4ac279e826f9",
+    "url": "/typetype/css/app.94967e38.css"
+  },
+  {
+    "revision": "7cc4430c4ac279e826f9",
+    "url": "/typetype/js/app.c3ffa09f.js"
+  },
+  {
+    "revision": "0a848ef250b059e47dbc",
+    "url": "/typetype/js/chunk-vendors.0a6cfac0.js"
+  },
+  {
+    "revision": "3da3bd3c02b22221d79f2a35f3cecd90",
+    "url": "/typetype/img/car2.3da3bd3c.png"
+  },
+  {
+    "revision": "d403a41c89be7b0cd84a211a4a7a86c5",
+    "url": "/typetype/img/flag.d403a41c.gif"
+  },
+  {
+    "revision": "359ed518e99971fdf5e1e470cb3eb5d9",
+    "url": "/typetype/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/typetype/robots.txt"
+  }
+];
